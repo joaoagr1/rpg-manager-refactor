14fcaab10ca627d71f34d324b31eaa81c26408e3
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2024-03-04 13:53:22 -0300

    :tada: Commit inicial

---
+++ src/main/java/rpg/manager/refactor/api/RpgManagerApplication.java
@@ -0,0 +1,13 @@
+package rpg.manager.refactor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rpg/manager/refactor/api/RpgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rpg.manager.refactor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
