156678d886f7152791f1db52e2ed713878856b56
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2024-03-07 14:58:38 -0300

    checkpoint

---
+++ src/main/java/rpg/manager/refactor/api/SubclassCreateDTO.java
@@ -0,0 +1,10 @@
+package rpg.manager.refactor.api;
+
+public record SubclassCreateDTO(
+        Integer classId,
+        String subClassName,
+        String subClassDescription
+
+
+) {
+}
--- src/main/java/rpg/manager/refactor/api/controller/SubclassController.java
+++ src/main/java/rpg/manager/refactor/api/controller/SubclassController.java
@@ -1,10 +1,11 @@
 package rpg.manager.refactor.api.controller;
 
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import rpg.manager.refactor.api.Subclass;
-import rpg.manager.refactor.api.SubclassRepository;
+import rpg.manager.refactor.api.*;
+import rpg.manager.refactor.api.Character;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,19 +26,33 @@ public class SubclassController {
 
 
     @GetMapping("/subclasses/{subclassId}")
-    public ResponseEntity<Optional<Subclass>> getSublassById(@PathVariable Integer subclassId){
+    public ResponseEntity<Optional<Subclass>> getSublassById(@PathVariable Integer subclassId) {
         Optional<Subclass> subclasses = subclassRepository.findById(subclassId);
         return ResponseEntity.ok().body(subclasses);
     }
 
-    @PostMapping("/subclasses")
-    public ResponseEntity<Subclass> createNewSubclass(@RequestBody Subclass newSubclass){
-        subclassRepository.save(newSubclass);
-        return ResponseEntity.ok().body(newSubclass);
+    @PostMapping("/subclasses/{subclassId}")
+    public ResponseEntity<Subclass> createNewSubclass(@RequestBody SubclassCreateDTO newSubclass) {
+
+        CharacterClass characterClass = subclassRepository.findById(newSubclass.())
+
+
+        if (characterUpdate.subclass() != null) {
+            Subclass subclass = subclassRepository.findById(characterUpdate.subclass())
+                    .orElseThrow(() -> new ResourceNotFoundException("Subclass not found with id: " + characterUpdate.subclass()));
+            character.setSubclass(subclass);
+            System.out.println(subclass);
+        }
+
+
+
+        characterRepository.save(character);
+
+        return ResponseEntity.ok(character);
     }
 
     @DeleteMapping("/subclasses/{subclassesId}")
-    public ResponseEntity<?> deleteSublass(@PathVariable Integer subClassId){
+    public ResponseEntity<?> deleteSublass(@PathVariable Integer subClassId) {
         subclassRepository.deleteById(subClassId);
         return ResponseEntity.noContent().build();
     }
